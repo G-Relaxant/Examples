e1ad47c7d83c45db12bc3e5a8c4ce241ebf94c8d
Danya <theincredibledanzel@yandex.ru>
2022-06-08 03:43:58 +0400

    Ex5 nado dodelat

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "Masha")
+{
+        Console.WriteLine("Ura, eto zhe MASHA");
+}
+else
+{
+        Console.Write("Привет, ");
+        Console
+}
