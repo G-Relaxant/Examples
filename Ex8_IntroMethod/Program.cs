68ad572f7b483f048722d4dde162936cc95566ac
Danya <theincredibledanzel@yandex.ru>
2022-06-11 17:01:26 +0400

    add Ex9 first array

--- Ex8_IntroMethod/Program.cs
+++ Ex8_IntroMethod/Program.cs
@@ -8,7 +8,7 @@
 
 
 int a1 = 15;
-int b1 = 21;
+int b1 = 2122;
 int c1 = 39;
 int a2 = 12;
 int b2 = 232;
@@ -17,9 +17,14 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 333;
 
-int max1 = Max(a1,b1,c1);
-int max2 = Max(a2,b2,c2);
-int max3 = Max(a3,b3,c3);
-int max = Max(max1,max2,max3);
+//int max1 = Max(a1,b1,c1);
+//int max2 = Max(a2,b2,c2);
+//int max3 = Max(a3,b3,c3);
+//int max = Max(max1,max2,max3);
 
+int max = Max(           //МОЖНО ОСТАВИТЬ И В ОДНОЙ СТРОКЕ
+    Max(a1,b1,c1),
+    Max(a2,b2,c2),
+    Max(a3,b3,c3));
+    
 Console.WriteLine(max);
