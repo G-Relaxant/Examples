dbb3ef004ef88dd52549af063e84fd0c532d6635
geekbrains <info@geekbrains.ru>
2022-06-24 18:27:50 +0400

    GoldenPeach

--- Ex13_RecursionAlgorithm/Program.cs
+++ Ex13_RecursionAlgorithm/Program.cs
@@ -42,7 +42,7 @@ for (int currentRow = 0; currentRow < row; currentRow++)
 
 
 
-/*
+
 int row = 20;
 int column = 22;
 
@@ -66,11 +66,19 @@ for (int currentRow = 0; currentRow < row; currentRow++)
 {
     for(int currentColumn = 0; currentColumn < column; currentColumn++)
     {
-        Console.Write($" {matrix[currentRow, currentColumn]} ");
+        //Console.Write($" {matrix[currentRow, currentColumn]} ");
+        if(matrix[currentRow, currentColumn] == 0)
+            {
+                Console.Write("   ");
+            }
+            else
+            {
+                Console.Write("(3)");
+            }
     }
     Console.WriteLine("");
 }
-*/
+
 
 
 
@@ -87,12 +95,14 @@ for (int i = 0; i < matrix.GetLength(0); i++)           // Новая коман
 
 
 
+// SLADKIY PERSIK
 
-int[,] matrix = new int[9, 12];
-PrintArray(matrix);
-Console.WriteLine();
-FillArray(matrix);
-PrintArray(matrix);
+int[,] Persik = new int[28, 37];
+//Console.WriteLine();
+//FillArray(matrix);
+GoldenPeach(Persik);
+//PrintArray(PersikPopka);
+PrintPeach(Persik);
 
 void FillArray(int[,] a)
 {
@@ -100,9 +110,135 @@ void FillArray(int[,] a)
     {
         for (int j = 0; j < a.GetLength(1); j++)
         {
-            matrix[i, j] = new Random().Next(1,10);
+            Persik[i, j] = new Random().Next(1,10);
         }
     }
+}
+
+void GoldenPeach(int[,] a)
+{
+    int b = 3;
+    //right line
+    a[27, 19] = b;
+    a[26, 20] = b;
+    a[26, 21] = b;
+    a[26, 22] = b;
+    a[25, 23] = b;
+    a[25, 24] = b;
+    a[25, 25] = b;
+    a[25, 26] = b;
+    a[24, 27] = b;
+    a[24, 28] = b;
+    a[24, 29] = b;
+    a[23, 30] = b;
+    a[22, 31] = b;
+    a[21, 32] = b;
+    a[20, 32] = b;
+    a[19, 33] = b;
+    a[18, 34] = b;
+    a[17, 34] = b;
+    a[16, 35] = b;
+    a[15, 35] = b;
+    a[14, 36] = b;
+    a[13, 36] = b;
+    a[12, 36] = b;
+    a[11, 36] = b;
+    a[10, 36] = b;
+    a[9, 35] = b;
+    a[8, 35] = b;
+    a[7, 35] = b;
+    a[6, 34] = b;
+    a[5, 34] = b;
+    a[4, 33] = b;
+    a[3, 33] = b;
+    a[2, 32] = b;
+    a[1, 31] = b;
+    a[1, 30] = b;
+    a[1, 29] = b;
+    a[0, 28] = b;
+    a[0, 28] = b;
+    a[0, 27] = b;
+    a[0, 26] = b;
+    a[0, 25] = b;
+    a[0, 24] = b;
+    a[1, 23] = b;
+    a[1, 22] = b;
+    a[1, 21] = b;
+    a[2, 20] = b;
+    //left line
+    a[26, 18] = b;
+    a[26, 17] = b;
+    a[26, 16] = b;
+    a[25, 15] = b;
+    a[25, 14] = b;
+    a[25, 13] = b;
+    a[24, 12] = b;
+    a[24, 11] = b;
+    a[24, 10] = b;
+    a[23, 9] = b;
+    a[23, 8] = b;
+    a[22, 7] = b;
+    a[22, 6] = b;
+    a[21, 4] = b;
+    a[21, 5] = b;
+    a[20, 3] = b;
+    a[19, 2] = b;
+    a[18, 2] = b;
+    a[17, 1] = b;
+    a[16, 1] = b;
+    a[15, 1] = b;
+    a[14, 0] = b;
+    a[13, 0] = b;
+    a[12, 0] = b;
+    a[11, 0] = b;
+    a[10, 0] = b;
+    a[9, 1] = b;
+    a[8, 1] = b;
+    a[7, 1] = b;
+    a[6, 2] = b;
+    a[5, 2] = b;
+    a[4, 2] = b;
+    a[3, 3] = b;
+    a[2, 4] = b;
+    a[2, 5] = b;
+    a[1, 6] = b;
+    a[1, 7] = b;
+    a[1, 8] = b;
+    a[1, 9] = b;
+    a[0, 10] = b;
+    a[0, 11] = b;
+    a[0, 12] = b;
+    a[0, 13] = b;
+    a[0, 14] = b;
+    a[0, 15] = b;
+    a[1, 16] = b;
+    a[1, 17] = b;
+    a[1, 18] = b;
+    a[1, 19] = b;
+    // midle line
+    a[25, 20] = b;
+    a[24, 21] = b;
+    a[23, 21] = b;
+    a[22, 22] = b;
+    a[21, 22] = b;
+    a[20, 23] = b;
+    a[19, 23] = b;
+    a[18, 24] = b;
+    a[17, 24] = b;
+    a[16, 24] = b;
+    a[15, 25] = b;
+    a[14, 25] = b;
+    a[13, 25] = b;
+    a[12, 25] = b;
+    a[11, 24] = b;
+    a[10, 24] = b;
+    a[9, 24] = b;
+    a[8, 23] = b;
+    a[7, 23] = b;
+    a[6, 23] = b;
+    a[5, 22] = b;
+    a[4, 22] = b;
+    a[3, 21] = b;
 }
 
 void PrintArray(int[,] a)
@@ -112,6 +248,26 @@ void PrintArray(int[,] a)
         for(int j = 0; j < a.GetLength(1); j++)
         {
             Console.Write($" {a[i, j]} ");
+        }
+    Console.WriteLine("");
+    }
+}
+
+void PrintPeach(int[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)          
+    {
+        for(int j = 0; j < a.GetLength(1); j++)
+        {
+            //Console.Write($" {a[i, j]} ");
+            if(a[i, j] == 0)
+            {
+                Console.Write("   ");
+            }
+            else
+            {
+                Console.Write("(3)");
+            }
         }
     Console.WriteLine("");
     }
