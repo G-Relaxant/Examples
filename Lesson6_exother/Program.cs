70b23201643dc92864622cf4965d5e140992fe29
geekbrains <info@geekbrains.ru>
2022-07-10 23:42:46 +0400

    edit

---
+++ Lesson6_exother/Program.cs
@@ -0,0 +1,111 @@
+﻿/*
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ").ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
+*/
+
+/*
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .ToArray()
+                .Select(item => item.Split(','))
+                .ToArray();
+///
+for (int i = 0; i < data.Length; i++)
+{
+    //Console.WriteLine(data[i]);
+    for (int k = 0; k < data[i].Length; k++)
+    {
+        Console.WriteLine(data[i][k]);
+    }
+}
+*/
+
+
+/*
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .ToArray()
+                .Select(item => item.Split(','))
+                .Select(arr => (x: int.Parse(arr[0]), y: int.Parse(arr[1])))
+                .ToArray();
+///
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i].x * 10);
+    Console.WriteLine();
+}
+*/
+
+
+/*
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .ToArray()
+                .Select(item => item.Split(','))
+                .Select(arr => (x: int.Parse(arr[0]), y: int.Parse(arr[1])))
+                .Select(point => (point.x * 10, point.y))
+                .ToArray();
+///
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    Console.WriteLine();
+}
+*/
+
+
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "")
+                ;
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .ToArray()
+                .Select(item => item.Split(','))
+                .Select(arr => (x: int.Parse(arr[0]), y: int.Parse(arr[1])))
+                .Where(arr => arr.x % 2 == 0)
+                .Select(point => (point.x * 10, point.y))
+                .ToArray();
+///
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+    Console.WriteLine();
+}
